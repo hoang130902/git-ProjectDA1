9fa65e44623e538011acfef0f7504eb58adb5e2d
Việt Hoàng <hoang130902@gmail.com>
2023-08-09 20:28:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ while cap.isOpened():
     if not success:
         break
     # Chuyển thành ảnh màu
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Đọc vào là ảnh RGB, ảnh mặc định của openCV là BGR 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Ảnh mặc định của openCV là BGR, ảnh đọc vào là ảnh RGB
     result = hands.process(img) # Đọc hình ảnh thành công
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #Sau khi chuyển sang RGB để xử lý, chuyển ngược lại BGR để hiển thị
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # Sau khi chuyển sang RGB để xử lý, chuyển ngược lại BGR để hiển thị
 
     alphabet = ''
     # Vẽ tọa độ khung xương bàn tay
